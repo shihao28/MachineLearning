8c1ffbcd24c8cd3f2253e164e46611350e084626
tan.shihao <tan.shihao@petronas.com>
2022-05-25 21:05:07 +0800

    Added predict pipeline config

--- config/config.py
+++ config/config.py
@@ -4,43 +4,65 @@ from sklearn.svm import *
 
 
 class Config(object):
-    data = dict(
+
+    # Training config
+    train = dict(
+
+        problem_type="classification",
+
+        data=dict(
+            data_path="data/01_raw/housing.csv",
+            label="above_median_house_value",
+        ),
+
+        train_val_test_split=dict(
+            split_ratio=0.3,
+        ),
+
+        model={
+            SVC.__name__: SVC(probability=True),
+            LogisticRegression.__name__: LogisticRegression()
+        },
+
+        param_grid={
+            "tune": False,
+            SVC.__name__: dict(
+                model__C=[1, 5],
+                model__kernel=["linear", "poly", "rbf"]
+                ),
+            LogisticRegression.__name__: dict(
+                model__penalty=["none", "l2"]
+            )
+        },
+
+        evaluation=dict(
+            # Get list of metrics from below
+            # https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
+            regression="r2",
+            classification="recall_weighted"
+        ),
+
+        mlflow=dict(
+            tracking_uri="http://127.0.0.1",
+            backend_uri="sqlite:///mlflow.db",
+            artifact_uri="./mlruns/",
+            experiment_name="Best Pipeline",
+            run_name="trial",
+            registered_model_name="my_cls_model",
+            port="5000",
+        ),
+    )
+
+    # Prediction config
+    predict = dict(
         data_path="data/01_raw/housing.csv",
-        label="above_median_house_value",
-    )
 
-    train_val_test_split = dict(
-        split_ratio=0.3,
-    )
-
-    model = {
-        SVC.__name__: SVC(probability=True),
-        LogisticRegression.__name__: LogisticRegression()
-    }
-
-    param_grid = {
-        "tune": False,
-        SVC.__name__: dict(
-            model__C=[1, 5],
-            model__kernel=["linear", "poly", "rbf"]
-            ),
-        LogisticRegression.__name__: dict(
-            model__penalty=["none", "l2"]
-        )
-    }
-
-    evaluation = dict(
-        # Get list of metrics from below
-        # https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
-        classification="recall_weighted"
-    )
-
-    mlflow = dict(
-        tracking_uri="http://127.0.0.1",
-        backend_uri="sqlite:///mlflow.db",
-        artifact_uri="./mlruns/",
-        experiment_name="Best Pipeline",
-        run_name="trial",
-        registered_model_name="my_cls_model",
-        port="5000",
+        mlflow=dict(
+            tracking_uri="http://127.0.0.1",
+            backend_uri="sqlite:///mlflow.db",
+            artifact_uri="./mlruns/",
+            model_name="my_cls_model",
+            port="5000",
+            model_version="latest"
+        ),
     )
